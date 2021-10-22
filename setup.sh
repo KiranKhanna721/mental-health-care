ab9735a7d14869cb3659a1ad5e977cfdca412c4e
KiranKhanna721 <76143695+KiranKhanna721@users.noreply.github.com>
2021-10-22 15:16:12 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "
+[general]n
+email = "khkiran01@gmail.com"n
+" > ~/.streamlit/credentials.toml
+echo "
+[server]n
+headless = truen
+enableCORS=falsen
+port = $PORTn
+" > ~/.streamlit/config.toml
